f735281aec27b7794bbd52cef82d84369d763a65
Tina <thywang1@gmail.com>
2023-10-09 12:38:11 -0400

    config: remove logging in production

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    esbuild: {
+      pure: mode === "production" ? ["console.log"] : [],
+    },
+    plugins: [react()],
+    test: {
+      globals: true,
+      environment: "jsdom",
+    },
+  };
 });
